f766362fea6d4fb34b9e23dd4c501f26694fc859
aratur <artur.jedrzejczak@gmail.com>
2020-09-07 23:25:36 +0200

    tests with eslint

--- src/tests/RangePicker.test.js
+++ src/tests/RangePicker.test.js
@@ -1,39 +1,40 @@
+/* eslint-env jest */
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import RangePicker from '../RangePicker';
 
-const renderRangePicker = (values, callback = () => {}) => {
-  return render(<RangePicker
+const renderRangePicker = (values, callback = () => {}) => render(
+  <RangePicker
     rangeValues={values}
     handleOnClick={callback}
-  />)
-};
+  />,
+);
 
 describe('RangePicker', () => {
   it('should render all selected values', () => {
-    const values = Array(10).fill(true)
-    const {getAllByRole} = renderRangePicker(values);
-    expect(getAllByRole('checkbox', {checked:true})).toHaveLength(10);
+    const values = Array(10).fill(true);
+    const { getAllByRole } = renderRangePicker(values);
+    expect(getAllByRole('checkbox', { checked: true })).toHaveLength(10);
   });
 
   it('should call parent onClick function which will update its state', () => {
     const onClose = jest.fn();
-    const values = Array(10).fill(true)
-    const {getAllByRole, getByText} = renderRangePicker(values, onClose);
+    const values = Array(10).fill(true);
+    const { getByText } = renderRangePicker(values, onClose);
     fireEvent.click(getByText('1'));
     fireEvent.click(getByText('2'));
     expect(onClose).toHaveBeenCalledTimes(2);
   });
 
   it('should have proper class based on received array', () => {
-    const values = Array(true, false, false, true);
-    const {getAllByRole} = renderRangePicker(values);
-    getAllByRole('checkbox').forEach(x => {
-      if (x.getAttribute('aria-checked') === "true"){
-        expect(x.getAttribute('class')).toBe('btn btn-success')}
-      else {
-        expect(x.getAttribute('class')).toBe('btn btn-info')}
+    const values = [true, false, false, true];
+    const { getAllByRole } = renderRangePicker(values);
+    getAllByRole('checkbox').forEach((x) => {
+      if (x.getAttribute('aria-checked') === 'true') {
+        expect(x).toHaveAttribute('class', 'btn btn-success');
+      } else {
+        expect(x).toHaveAttribute('class', 'btn btn-info');
       }
-    );
-  })
+    });
+  });
 });
