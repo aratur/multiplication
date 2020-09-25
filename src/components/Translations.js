e688005197c00c03ac850063bed74cfe9c2e2ffc
aratur <artur.jedrzejczak@gmail.com>
2020-09-25 11:26:05 +0200

    transaltions vbeta

--- src/components/Translations.js
+++ src/components/Translations.js
@@ -1,3 +1,4 @@
+/* eslint class-methods-use-this: 0 */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
@@ -9,8 +10,8 @@ class Translations extends Component {
     this.fetchTranslations('pl');
     this.fetchTranslations('en');
     this.state = {
-      pl: {},
-      en: {},
+      pl: undefined,
+      en: undefined,
       currentLanguage: language,
     };
     this.i18n = this.i18n.bind(this);
@@ -26,29 +27,38 @@ class Translations extends Component {
       end = statement.indexOf('}', start);
       const key = statement.substring(start, end);
       console.log(key);
-      if (Number(key) || key === '0') keys.push(key);
+      if (Number(key) || key === '0') keys.push(Number(key));
     }
-    return { keys };
+    return keys;
   }
 
-  test() {
-    const person = 'Artur';
-    const age = 35;
-    const text = this.i18n('That {0} is a {1} years old', person, age);
-    console.log(text);
-  }
-
-  i18n(statement, ...values) {
+  i18n(statement, ...parameters) {
     const keys = this.getKeysFromStatement(statement);
+    const values = [...parameters];
 
     const { currentLanguage, pl, en } = this.state;
     const dictionary = currentLanguage === 'pl' ? pl : en;
-    if (dictionary === 'undefined') return null;
+    if (typeof dictionary === 'undefined') {
+      console.warn('Could not translate the entry because of empty dictionary ', currentLanguage);
+      return null;
+    }
 
-    console.log(keys);
-    console.log(values);
-    console.log(dictionary.equation.label);
-    return '';
+    let resultString = statement.indexOf('.') === -1 ? statement
+      : statement.split('.')
+        .reduce((result, step) => result[step], dictionary);
+    if (typeof resultString === 'undefined') {
+      console
+        .warn(`Could not retrieve an entry on path "${statement}" in dictionary "${currentLanguage}"`);
+    }
+
+    if (typeof keys === 'undefined') return resultString;
+    keys.forEach((key) => {
+      if (values !== 'undefined' && values.length <= key) {
+        resultString = resultString.replace(`{${key}}`, values[key]);
+      }
+    });
+
+    return resultString;
   }
 
   fetchTranslations(language) {
@@ -60,7 +70,6 @@ class Translations extends Component {
         this.setState(
           languageTranslations,
         );
-        this.test();
       });
   }
 
@@ -71,7 +80,7 @@ class Translations extends Component {
       const newChildren = React.Children
         .map(children, (child) => this.addTranslation(child));
       return React
-        .cloneElement(element, { alt: 'level', children: newChildren });
+        .cloneElement(element, { i18n: this.i18n, children: newChildren });
     }
     return element;
   }
@@ -85,7 +94,7 @@ class Translations extends Component {
 Translations.propTypes = {
   language: PropTypes.oneOf(['pl', 'en']).isRequired,
   children: PropTypes.oneOfType(
-    [PropTypes.string, PropTypes.func],
+    [PropTypes.string, PropTypes.func, PropTypes.object],
   ).isRequired,
 };
 
