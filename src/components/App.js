41a6bee8bdfaf434d6e0b8bca000b4ce986122a4
Artur J <artur.jedrzejczak@gmail.com>
2023-02-27 11:55:51 +0100

    lazy suspense

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import store from '../redux-store/store';
-import Settings from './Settings';
 import LandingPage from './LandingPage';
-import ResultsTable from './ResultsTable';
 import Navbar from './Navbar';
 import Gems from './Gems';
+import Loader from './Loader';
 
 const styleRowMargin = { marginLeft: 0, marginRight: 0 };
 const styleColPadding = { paddingLeft: '5px', paddingRight: '5px' };
+
+const ResultsTableLazy = lazy(() => import('./ResultsTable'));
+const SettingsLazy = lazy(() => import('./Settings'));
 
 const App = () => (
   <Provider store={store}>
@@ -25,11 +27,13 @@ const App = () => (
       <div className="row" style={styleRowMargin}>
         <div className="col-md-3 col-sm-2" />
         <div className="col-md-6 col-sm-8 text-center" style={styleColPadding}>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/results" element={<ResultsTable />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/results" element={<ResultsTableLazy />} />
+              <Route path="/settings" element={<SettingsLazy />} />
+            </Routes>
+          </Suspense>
         </div>
         <div className="col-md-3 col-sm-2" />
       </div>
